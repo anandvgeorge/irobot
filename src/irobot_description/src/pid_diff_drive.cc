144e437252acc3a5b21d5c88da240fc0e0904180
Anand George <anandvgeorge@gmail.com>
2019-12-31 19:09:54 +0200

    Gets the robot pose in plugin

--- src/irobot_description/src/pid_diff_drive.cc
+++ src/irobot_description/src/pid_diff_drive.cc
@@ -4,6 +4,12 @@
 #include <gazebo/common/common.hh>
 #include <ignition/math/Vector3.hh>
 #include <ros/ros.h>
+#include <gazebo/transport/transport.hh>
+#include <gazebo/msgs/msgs.hh>
+
+
+// ros::Rate loop_rate(5);
+ignition::math::Pose3d robot_pose;
 
 namespace gazebo
 {
@@ -18,11 +24,23 @@ namespace gazebo
       // Store the pointer to the model
       this->model = _parent;
 
+      // gazebo::math::Pose robot_pose;
+      // float yaw;
+      
+
       // Listen to the update event. This event is broadcast every
       // simulation iteration.
       this->updateConnection = event::Events::ConnectWorldUpdateBegin(
           std::bind(&PIDDiffDrive::OnUpdate, this));
       ROS_WARN("PIDDiffDrive plugin loaded with %s", this->model->GetName().c_str());
+
+      // Create transport node
+      this->node = transport::NodePtr(new transport::Node());
+
+      std::string models_state = "/gazebo/model_states";
+
+      // Subscribe to topic
+      // this->sub = this->node->Subscribe(models_state, PIDDiffDrive::OnMsg, this, false);
     }
 
     // Called by the world update start event
@@ -32,16 +50,38 @@ namespace gazebo
       // this->model->SetLinearVel(ignition::math::Vector3d(.3, 0, 0));
 
       // Mark the existance
-      ROS_INFO("OnUpdate");
+      // ROS_INFO("OnUpdate");
 
       // Get pose of the model
+      robot_pose = this->model->WorldPose();
+      // yaw = robot_pose.GetYaw()
+      ignition::math::Vector3d pos = robot_pose.Pos();
+      ignition::math::Quaterniond rot = robot_pose.Rot();
+      float x = pos.X();
+      float y = pos.Y();
+      float yaw = rot.Yaw();
+      // ROS_INFO("Pose of %s is %f, %f and %f", this->model->GetName().c_str(), x, y, yaw);
 
       // Get pose of the target
 
       // Compute the velocities with PID
 
       // Publish to cmd_vel
+
+      // Sleep
+      // loop_rate.sleep();
     }
+
+    public: void OnMsg(physics::ModelPtr &_msg)
+    {
+      ROS_INFO("Please work");
+    }
+
+    // node used for transport
+    private: transport::NodePtr node;
+
+    // subscriber to a named topic.
+    private: transport::SubscriberPtr sub;
 
     // Pointer to the model
     private: physics::ModelPtr model;
