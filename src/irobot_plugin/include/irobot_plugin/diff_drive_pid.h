1798e27f05fb0178e9f13419bf568e557234b586
Anand George <anandvgeorge@gmail.com>
2020-01-03 02:10:46 +0200

    Removes test plugins and adds diff_drive plugin back

--- src/irobot_plugin/include/irobot_plugin/diff_drive_pid.h
+++ src/irobot_plugin/include/irobot_plugin/diff_drive_pid.h
@@ -15,49 +15,49 @@ namespace gazebo
 {
 class DiffDrivePID : public ModelPlugin
 {
-	public:
-		DiffDrivePID();
-		virtual ~DiffDrivePID();
+    public:
+        DiffDrivePID();
+        virtual ~DiffDrivePID();
 
-		virtual void Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf );
+        virtual void Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf );
 
-	protected:
-		virtual void UpdateChild();
+    protected:
+        virtual void UpdateChild();
 
-	private:
-		void getPose();
-		void publishVelocity();
-		void modelStateCallback( const gazebo_msgs::ModelStates::ConstPtr& _msg );
-		
-		/// Parameters
-		std::string node_namespace_;
-		std::string velocity_topic_;
-		std::string model_state_topic_;
-		std::string update_rate_;
+    private:
+        void getPose();
+        void publishVelocity();
+        void modelStateCallback( const gazebo_msgs::ModelStates::ConstPtr& _msg );
+        
+        /// Parameters
+        std::string node_namespace_;
+        std::string velocity_topic_;
+        std::string model_state_topic_;
+        std::string update_rate_;
 
-		// ROS STUFF
-		ros::NodeHandle *rosnode_;
-		//ros::Service operating_mode_srv_;
+        // ROS STUFF
+        ros::NodeHandle *rosnode_;
+        //ros::Service operating_mode_srv_;
 
-		ros::Publisher velocity_publisher_;
-		ros::Subscriber model_state_subscriber_;
-		geometry_msgs::Twist velocity_;
+        ros::Publisher velocity_publisher_;
+        ros::Subscriber model_state_subscriber_;
+        geometry_msgs::Twist velocity_;
 
-		physics::WorldPtr my_world_;
-		physics::ModelPtr my_parent_;
+        physics::WorldPtr my_world_;
+        physics::ModelPtr my_parent_;
 
-		// Simulation time of the last update
-		common::Time prev_update_time_;
+        // Simulation time of the last update
+        common::Time prev_update_time_;
 
-		void spin();
-		boost::thread *spinner_thread_;
-		boost::mutex lock;		
+        void spin();
+        boost::thread *spinner_thread_;
+        boost::mutex lock;		
 
-		transport::NodePtr gazebo_node_;
-		transport::SubscriberPtr contact_sub_;
+        transport::NodePtr gazebo_node_;
+        transport::SubscriberPtr contact_sub_;
 
-		// Pointer to the update event connection
-		event::ConnectionPtr updateConnection;
+        // Pointer to the update event connection
+        event::ConnectionPtr updateConnection;
 };
 }
 #endif
--- src/irobot_plugin/src/diff_drive_pid.cpp
+++ src/irobot_plugin/src/diff_drive_pid.cpp
@@ -12,96 +12,94 @@ using namespace gazebo;
 
 DiffDrivePID::DiffDrivePID() : gazebo_node_(new transport::Node())
 {
-	this->spinner_thread_ = new boost::thread( boost::bind( &DiffDrivePID::spin, this) );
+    this->spinner_thread_ = new boost::thread( boost::bind( &DiffDrivePID::spin, this) );
 }
 
 DiffDrivePID::~DiffDrivePID()
 {
-	rosnode_->shutdown();
-	this->spinner_thread_->join();
-	delete this->spinner_thread_;
-	//   delete [] wheel_speed_;
-	delete rosnode_;
+    rosnode_->shutdown();
+    this->spinner_thread_->join();
+    delete this->spinner_thread_;
+    //   delete [] wheel_speed_;
+    delete rosnode_;
 }
 
 void DiffDrivePID::Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf )
 {
-	this->my_world_ = _parent->GetWorld();
+    this->my_world_ = _parent->GetWorld();
 
-	this->my_parent_ = _parent;
-	if (!this->my_parent_)
-	{
-		ROS_FATAL("rosnode_Create controller requires a Model as its parent");
-		return;
-	}
+    this->my_parent_ = _parent;
+    if (!this->my_parent_)
+    {
+        ROS_FATAL("rosnode_Create controller requires a Model as its parent");
+        return;
+    }
 
-	this->node_namespace_ = "";
-	if (_sdf->HasElement("node_namespace"))
-		this->node_namespace_ = _sdf->GetElement("node_namespace")->Get<std::string>() + "/";
+    this->node_namespace_ = "";
+    if (_sdf->HasElement("node_namespace"))
+        this->node_namespace_ = _sdf->GetElement("node_namespace")->Get<std::string>() + "/";
 
 
-	// if (!ros::isInitialized())
-	// {
-	// 	int argc = 0;
-	// 	char** argv = NULL;
-	// 	ros::init(argc, argv, "irobot_gazebo", ros::init_options::NoSigintHandler|ros::init_options::AnonymousName);
-	// }
-	ros::isInitialized();
+    // if (!ros::isInitialized())
+    // {
+    // 	int argc = 0;
+    // 	char** argv = NULL;
+    // 	ros::init(argc, argv, "irobot_gazebo", ros::init_options::NoSigintHandler|ros::init_options::AnonymousName);
+    // }
+    ros::isInitialized();
 
-	rosnode_ = new ros::NodeHandle( node_namespace_ );
+    rosnode_ = new ros::NodeHandle( node_namespace_ );
 
-	rosnode_->param<std::string> ( "velocityTopic", velocity_topic_, "/cmd_vel" );
-    rosnode_->param<std::string> ( "modelStateTopic", model_state_topic_, "/gazebo/model_states" );
+    rosnode_->param<std::string> ( "velocityTopic", velocity_topic_, "cmd_vel" );
+    rosnode_->param<std::string> ( "modelStateTopic", model_state_topic_, "gazebo/model_states" );
 
-	// velocity_topic_ = rosnode_->param()
+    model_state_subscriber_ = rosnode_->subscribe(model_state_topic_, 1, &DiffDrivePID::modelStateCallback, this );
+    if ( model_state_subscriber_ )
+        ROS_INFO("DiffDrivePID: Subscribe to %s", model_state_topic_.c_str());
 
-	model_state_subscriber_ = rosnode_->subscribe(model_state_topic_, 1, &DiffDrivePID::modelStateCallback, this );
-	if ( model_state_subscriber_ )
-    	ROS_INFO_NAMED("diff_drive_pid", "Subscribe to %s", model_state_topic_.c_str());
+    velocity_publisher_ = rosnode_->advertise<geometry_msgs::Twist>(velocity_topic_, 1);
+    if ( velocity_publisher_ )
+        ROS_INFO("DiffDrivePID: Advertise velocity on %s ", velocity_topic_.c_str());
 
-	velocity_publisher_ = rosnode_->advertise<geometry_msgs::Twist>(velocity_topic_, 1);
-	if ( velocity_publisher_ )
-		ROS_INFO_NAMED("diff_drive_pid", "Advertise velocity on %s ", velocity_topic_.c_str());
+    // Get then name of the parent model
+    std::string model_name = _sdf->GetParent()->Get<std::string>("name");
 
-	// Get then name of the parent model
-	std::string model_name = _sdf->GetParent()->Get<std::string>("name");
+    // Listen to the update event. This event is broadcast every
+    // simulation iteration.
+    this->updateConnection = event::Events::ConnectWorldUpdateBegin(boost::bind(&DiffDrivePID::UpdateChild, this));
 
-	// Listen to the update event. This event is broadcast every
-	// simulation iteration.
-	this->updateConnection = event::Events::ConnectWorldUpdateBegin(boost::bind(&DiffDrivePID::UpdateChild, this));
-
-	ROS_INFO("Model name: %s", model_name.c_str());
-	// gzdbg << "plugin model name: " << model_name << "\n";
+    ROS_INFO("Model name: %s", model_name.c_str());
+    // gzdbg << "plugin model name: " << model_name << "\n";
 }
 
 void DiffDrivePID::UpdateChild()
 {
 
-	publishVelocity();
+    publishVelocity();
 
-	// Update the pose data from the simulator
-	getPose();
+    // Update the pose data from the simulator
+    getPose();
 
-	// The main algorithm goes here. If needed, call other function(s)
+    // The main algorithm goes here. If needed, call other function(s)
 }
 
 void DiffDrivePID::getPose()
 {
     boost::mutex::scoped_lock scoped_lock ( lock );
-	ROS_DEBUG("Mutex locked for getPose");
+    ROS_DEBUG("Mutex locked for getPose");
 
-	// Get pose of the robot and target
+    // Get pose of the robot and target
 
 }
 
 void DiffDrivePID::modelStateCallback ( const gazebo_msgs::ModelStates::ConstPtr& _msg )
 {
     boost::mutex::scoped_lock scoped_lock ( lock );
-	ROS_DEBUG("Mutex locked for modelStateCallback");
+    ROS_DEBUG("Mutex locked for modelStateCallback");
     // x_ = cmd_msg->linear.x;
     // rot_ = cmd_msg->angular.z;
 
-	// Do something useful here
+    // Do something useful here
 }
 
 void DiffDrivePID::publishVelocity ()
@@ -112,7 +110,7 @@ void DiffDrivePID::publishVelocity ()
 
 void DiffDrivePID::spin()
 {
-	while(ros::ok()) ros::spinOnce();
+    while(ros::ok()) ros::spinOnce();
 }
 
 GZ_REGISTER_MODEL_PLUGIN(DiffDrivePID);
